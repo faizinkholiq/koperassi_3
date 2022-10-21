2daa73190617a28eee582add8170800d4c154a7c
faizin <nasrunfaizinkholiq@gmail.com>
2022-10-21 13:01:14 +0700

    update migrations

--- migrations/003_anggota.sql
+++ migrations/003_anggota.sql
@@ -1,3 +1,3 @@
-CREATE TABLE `koperassi`.`person_family` (`id` SMALLINT NOT NULL AUTO_INCREMENT , `person_id` SMALLINT NOT NULL , `name` VARCHAR(60) NOT NULL , `address` TEXT NULL , `phone` VARCHAR(15) NULL , `status` VARCHAR(30) NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
+CREATE TABLE `person_family` (`id` SMALLINT NOT NULL AUTO_INCREMENT , `person_id` SMALLINT NOT NULL , `name` VARCHAR(60) NOT NULL , `address` TEXT NULL , `phone` VARCHAR(15) NULL , `status` VARCHAR(30) NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
 
 ALTER TABLE `person` ADD `join_date` DATE NULL AFTER `avatar`, ADD `status` ENUM('Aktif','Tidak Aktif') NOT NULL DEFAULT 'Tidak Aktif' AFTER `join_date`;
