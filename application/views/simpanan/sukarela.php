accb9d95c352a1f96a440faf2dce1d01831e1971
faizin <nasrunfaizinkholiq@gmail.com>
2022-11-03 01:47:02 +0700

    update form input simpanan wajib & sukarela

--- application/views/simpanan/sukarela.php
+++ application/views/simpanan/sukarela.php
@@ -1,11 +1,23 @@
 <link href="<?= base_url('assets/vendor/datatables/dataTables.bootstrap4.min.css') ?>" rel="stylesheet">
 
+<?php
+    if(!empty($this->session->flashdata('msg'))):
+        $msg = $this->session->flashdata('msg');
+?>
+<div class="alert <?= ($msg['success'])? 'alert-success' : 'alert-danger' ?> alert-dismissible fade show" role="alert">
+  <strong><?= $msg['message'] ?></strong>
+  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    <span aria-hidden="true">&times;</span>
+  </button>
+</div>
+<?php endif; ?>
+
 <div class="card shadow mb-4">
     <div class="card-header py-3">
         <h6 class="m-0 font-weight-bold text-primary">Data Simpanan Sukarela</h6>
     </div>
     <div class="card-body">
-        <a href="<?= site_url('simpanan/input/sukarela') ?>" class="btn my-btn-primary mr-2"><i class="fas fw fa-plus mr-1"></i> Tambah baru</a>
+        <a href="#!" class="btn my-btn-primary mr-2" onclick="showForm()"><i class="fas fw fa-plus mr-1"></i> Tambah baru</a>
         <a href="#!" class="btn btn-danger"><i class="fas fw fa-file-import mr-1"></i> Import Data</a>
         <hr>
         <div class="table-responsive">
@@ -39,14 +51,150 @@
     </div>
 </div>
 
+ <!-- Input Modal-->
+ <div class="modal fade" id="inputModal" tabindex="-1" role="dialog" aria-labelledby="inputModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="inputModalLabel"><i class="mr-2 fas fa-hand-holding-usd"></i> Tambah Simpanan</h5>
+                <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">×</span>
+                </button>
+            </div>
+            <form id="formSimpanan" action="<?= site_url('simpanan/create/'.$module) ?>" method="POST" enctype="multipart/form-data">
+            <div class="modal-body">
+                <div class="row mb-4 mt-4">
+                    <div class="col-lg-12">
+                        <div class="row mb-3">
+                            <div class="col-lg-3">Tanggal Bayar</div>
+                            <div class="col-lg-1 text-right">:</div>
+                            <div class="col-lg-5">
+                                <input type="date" class="form-control form-control-user" id="tglDateInput" name="date" 
+                                    value="<?=(isset($data["join_date"]) && !empty($data["join_date"]))? $data["join_date"] : date('Y-m-d') ?>">
+                            </div>
+                        </div>
+                        <div class="row mb-3">
+                            <div class="col-lg-3">No. Anggota</div>
+                            <div class="col-lg-1 text-right">:</div>
+                            <div class="col-lg-7">
+                                <input type="text" class="form-control" id="noAnggotaTextInput" name="no_anggota" readonly="readonly">
+                            </div>
+                        </div>
+                        <div class="row mb-3">
+                            <div class="col-lg-3">Nama Anggota</div>
+                            <div class="col-lg-1 text-right">:</div>
+                            <div class="col-lg-7">
+                                <select id="anggotaSelect" name="person" data-live-search="true" class="selectpicker form-control form-control-user">
+                                    <option value="">- Please Select -</option>
+                                    <?php foreach($person_list as $key => $item): ?>
+                                    <option value="<?= $item["id"] ?>"><?= $item["name"] ?></option>
+                                    <?php endforeach; ?>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="row mb-3">
+                            <div class="col-lg-3">Jabatan</div>
+                            <div class="col-lg-1 text-right">:</div>
+                            <div class="col-lg-7">
+                                <input type="text" class="form-control" id="jabatanTextInput" name="jabatan" readonly="readonly">
+                            </div>
+                        </div>
+                        <div class="row mb-3">
+                            <div class="col-lg-3">Depo/Stock Point</div>
+                            <div class="col-lg-1 text-right">:</div>
+                            <div class="col-lg-7">
+                                <input type="text" class="form-control" id="depoTextInput" name="depo" readonly="readonly">
+                            </div>
+                        </div>
+                        <div class="row mb-3">
+                            <div class="col-lg-3">No. Rekening</div>
+                            <div class="col-lg-1 text-right">:</div>
+                            <div class="col-lg-7">
+                                <input type="text" class="form-control" id="noRekTextInput" name="no_rek" readonly="readonly">
+                            </div>
+                        </div>
+                        <div class="row mb-3">
+                            <div class="col-lg-3">Alamat</div>
+                            <div class="col-lg-1 text-right">:</div>
+                            <div class="col-lg-7">
+                                <textarea readonly="readonly" class="form-control form-control-user" name="alamat" id="alamatTextArea"></textarea>
+                            </div>
+                        </div>
+                        <div class="row mb-3">
+                            <div class="col-lg-3">Tipe</div>
+                            <div class="col-lg-1 text-right">:</div>
+                            <div class="col-lg-5">
+                                <select class="form-control form-control-user" id="statusCombo" name="status" disabled="disabled">
+                                    <option value="pokok" <?=(isset($module) && $module == 'pokok')? 'selected' : '' ?>>Simpanan Pokok</option>
+                                    <option value="wajib" <?=(isset($module) && $module == 'wajib')? 'selected' : '' ?>>Simpanan Wajib</option>
+                                    <option value="sukarela" <?=(isset($module) && $module == 'sukarela')? 'selected' : '' ?>>Simpanan Sukarela</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="row mb-3">
+                            <div class="col-lg-3">Simpanan</div>
+                            <div class="col-lg-1 text-right">:</div>
+                            <div class="col-lg-5">
+                                <div class="input-group mb-2">
+                                    <div class="input-group-prepend">
+                                        <div class="input-group-text">Rp</div>
+                                    </div>
+                                    <input type="text" class="form-control" id="jumlahTextInput" name="balance" placeholder="...">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-danger mt-4 mb-4" data-dismiss="modal">Batal</button>
+                <button type="button" class="btn btn-info mt-4 mb-4" onclick="resetForm()">Reset</button>
+                <button type="submit" class="btn btn-success mt-4 mb-4 ml-2 mr-4"> Submit Simpanan <i class="ml-2 fas fa-chevron-right"></i></button>
+            </div>
+            </form>
+        </div>
+    </div>
+</div>
+
 <script src="<?= base_url('assets/vendor/datatables/jquery.dataTables.min.js') ?>"></script>
 <script src="<?= base_url('assets/vendor/datatables/dataTables.bootstrap4.min.js') ?>"></script>
 
 <script>
+    const url = {
+        base: '<?=base_url() ?>',
+        site: '<?=site_url() ?>'
+    };
 
-    // Call the dataTables jQuery plugin
+    const list_anggota = <?= json_encode($person_list); ?>
+
     $(document).ready(function() {
+        $('.alert').alert()
         $('#simpananTable').DataTable();
+
+        $("#anggotaSelect").change(function () {
+            let person_id = this.value;
+            let person = list_anggota.filter(r => r.id == person_id)
+
+            if(person.length > 0){
+                $('#noAnggotaTextInput').val(person[0].tmk)
+                $('#jabatanTextInput').val(person[0].position)
+                $('#depoTextInput').val(person[0].depo)
+                $('#alamatTextArea').text(person[0].address)
+                $('#noRekTextInput').val(person[0].acc_no)
+            }
+        });
+
     });
+
+    function showForm(){
+        $('#inputModal').modal('show');
+        $('#formSimpanan')[0].reset();
+        $('#alamatTextArea').text("")
+    }
+
+    function resetForm() {
+        $('#formSimpanan')[0].reset();
+        $('#alamatTextArea').text("")
+    }
 
 </script>
