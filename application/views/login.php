4b1ee81e0bc20caf6736643821f330fc1564de2a
faizin <nasrunfaizinkholiq@gmail.com>
2022-11-05 09:17:19 +0700

    remove trash js

--- application/views/login.php
+++ application/views/login.php
@@ -94,9 +94,6 @@
     <!-- Core plugin JavaScript-->
     <script src="<?= base_url('assets/vendor/jquery-easing/jquery.easing.min.js') ?>"></script>
 
-    <!-- Custom scripts for all pages-->
-    <script src="<?= base_url('js/sb-admin-2.min.js') ?>"></script>
-
 </body>
 
 </html>
