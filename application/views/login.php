0b5a64d5b5a0c430ddae541d5329331c5d112d8c
faizin <nasrunfaizinkholiq@gmail.com>
2022-11-05 09:18:39 +0700

    fix broken link

--- application/views/login.php
+++ application/views/login.php
@@ -72,10 +72,10 @@
                             </form>
                             <hr>
                             <div class="text-center">
-                                <a class="small" href="forgot-password.html">Forgot Password?</a>
+                                <a class="small" href="#!">Forgot Password?</a>
                             </div>
                             <div class="text-center">
-                                <a class="small" href="register.html">Create an Account!</a>
+                                <a class="small" href="#!">Create an Account!</a>
                             </div>
                         </div>
                     </div>
