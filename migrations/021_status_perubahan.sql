945511b9aa49a9439bbd489065d3e818597e628e
faizin <nasrunfaizinkholiq@gmail.com>
2022-12-01 23:38:46 +0700

    update ktp

--- migrations/021_status_perubahan.sql
+++ migrations/021_status_perubahan.sql
@@ -1,2 +1,2 @@
-ALTER TABLE `person_temp` ADD `status` ENUM("Pending", "Approved", "Rejected") NOT NULL AFTER `ktp_action`;
+ALTER TABLE `person_temp` ADD `status` ENUM("Pending", "Approved", "Rejected") NOT NULL AFTER `ktp`;
 ALTER TABLE `person_temp` ADD `reason` TEXT NULL AFTER `status`;
