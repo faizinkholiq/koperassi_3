c9cc3eb57596b57e925023038ca3bc0155c60e71
faizin <nasrunfaizinkholiq@gmail.com>
2022-12-31 03:17:05 +0700

    update new column year & month

--- migrations/030_new_update_simpanan.sql
+++ migrations/030_new_update_simpanan.sql
@@ -7,3 +7,8 @@ ALTER TABLE `simpanan_pokok` ADD `posting` BOOLEAN NOT NULL DEFAULT FALSE AFTER 
 ALTER TABLE `simpanan_wajib` ADD `posting` BOOLEAN NOT NULL DEFAULT FALSE AFTER `balance`;
 ALTER TABLE `simpanan_sukarela` ADD `posting` BOOLEAN NOT NULL DEFAULT FALSE AFTER `balance`;
 ALTER TABLE `investasi` ADD `posting` BOOLEAN NOT NULL DEFAULT FALSE AFTER `balance`;
+
+ALTER TABLE `simpanan_pokok` ADD `posting_date` DATE NULL AFTER `posting`;
+ALTER TABLE `simpanan_wajib` ADD `posting_date` DATE NULL AFTER `posting`;
+ALTER TABLE `simpanan_sukarela` ADD `posting_date` DATE NULL AFTER `posting`;
+ALTER TABLE `investasi` ADD `posting_date` DATE NULL AFTER `posting`;
