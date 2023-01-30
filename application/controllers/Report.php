c93a1a81d95c5dc97504d5ecd367b757dafca7a6
faizin <nasrunfaizinkholiq@gmail.com>
2023-01-30 16:40:32 +0700

    update report

--- application/controllers/Report.php
+++ application/controllers/Report.php
@@ -72,26 +72,76 @@ class Report extends CI_Controller {
     {
         $spreadsheet = new Spreadsheet();
         $sheet = $spreadsheet->getActiveSheet();
-        $sheet->setCellValue('A1', 'No');
-        $sheet->setCellValue('B1', 'Nama');
-        $sheet->setCellValue('C1', 'Kelas');
-        $sheet->setCellValue('D1', 'Jenis Kelamin');
-        $sheet->setCellValue('E1', 'Alamat');
+
+        $rowNo = 1;
+        $sheet->setCellValue("A$rowNo", 'Koperasi PT. Putri Daya Usahatama');
+        $rowNo++;
+        $sheet->setCellValue("A$rowNo", 'Rincian Simpanan Anggota Koperasi');
+        $rowNo+=2;
+
+        $firstRow = $rowNo;
+        $sheet->setCellValue("A$rowNo", 'NIK');
+        $sheet->getColumnDimension('A')->setWidth(20);
+        $sheet->mergeCells("A$rowNo:A".$rowNo+1);
+        $sheet->setCellValue("B$rowNo", 'Nama');
+        $sheet->getColumnDimension('B')->setWidth(35);
+        $sheet->mergeCells("B$rowNo:B".$rowNo+1);
+        $sheet->setCellValue("C$rowNo", 'Depo');
+        $sheet->getColumnDimension('C')->setWidth(20);
+        $sheet->mergeCells("C$rowNo:C".$rowNo+1);
+        $sheet->setCellValue("D$rowNo", 'Jabatan');
+        $sheet->getColumnDimension('D')->setWidth(35);
+        $sheet->mergeCells("D$rowNo:D".$rowNo+1);
+        $sheet->setCellValue("E$rowNo", 'Data Simpanan');
+        $sheet->mergeCells("E$rowNo:I$rowNo");
+        $rowNo++;
+        $sheet->setCellValue("E$rowNo", 'Wajib');
+        $sheet->getColumnDimension('E')->setWidth(20);
+        $sheet->setCellValue("F$rowNo", 'Pokok');
+        $sheet->getColumnDimension('F')->setWidth(20);
+        $sheet->setCellValue("G$rowNo", 'Sukarela');
+        $sheet->getColumnDimension('G')->setWidth(20);
+        $sheet->setCellValue("H$rowNo", 'Investasi');
+        $sheet->getColumnDimension('H')->setWidth(20);
+        $sheet->setCellValue("I$rowNo", 'Total');
+        $sheet->getColumnDimension('I')->setWidth(20);
+        $rowNo++;
         
-        // $siswa = $this->siswa_model->getAll();
-        // $no = 1;
-        // $x = 2;
-        // foreach($siswa as $row)
-        // {
-        //     $sheet->setCellValue('A'.$x, $no++);
-        //     $sheet->setCellValue('B'.$x, $row->nama);
-        //     $sheet->setCellValue('C'.$x, $row->kelas);
-        //     $sheet->setCellValue('D'.$x, $row->jenis_kelamin);
-        //     $sheet->setCellValue('E'.$x, $row->alamat);
-        //     $x++;
-        // }
+        $data = $this->report_model->get_data();
+        foreach($data as $row)
+        {
+            $sheet->setCellValue("A$rowNo", $row['nik']);
+            $sheet->setCellValue("B$rowNo", $row['name']);
+            $sheet->setCellValue("C$rowNo", $row['depo']);
+            $sheet->setCellValue("D$rowNo", $row['position']);
+            $sheet->setCellValue("E$rowNo", $row['wajib']);
+            $sheet->setCellValue("F$rowNo", $row['pokok']);
+            $sheet->setCellValue("G$rowNo", $row['sukarela']);
+            $sheet->setCellValue("H$rowNo", $row['investasi']);
+            $sheet->setCellValue("I$rowNo", $row['total']);
+            $rowNo++;
+        }
+        $rowNo--;
+
+        $styleArray = [
+            'font' => [
+                'bold' => false,
+            ],
+            'alignment' => [
+                'horizontal' => \PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_LEFT,
+                'vertical' => \PhpOffice\PhpSpreadsheet\Style\Alignment::VERTICAL_CENTER,
+            ],
+            'borders' => [
+                'outline' => array(
+                    'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN,
+                ),
+            ],
+        ];
+        
+        $sheet->getStyle("A$firstRow:I$rowNo")->applyFromArray($styleArray);
+
         $writer = new Xlsx($spreadsheet);
-        $filename = 'laporan-siswa';
+        $filename = 'Report_Simpanan_'.date('YmdHis');
         
         header('Content-Type: application/vnd.ms-excel');
         header('Content-Disposition: attachment;filename="'. $filename .'.xlsx"'); 
--- application/models/Report_model.php
+++ application/models/Report_model.php
@@ -140,5 +140,44 @@ class Report_model extends CI_Model {
         return $data;
     }
 
+    public function get_data() 
+    {
+        $this->db->select([
+            "person.id",
+            "person.nik",
+            "person.name",
+            "person.depo depo_id",
+            "depo.code depo_code",
+            "depo.name depo",
+            "person.position position_id",
+            "position.name position",
+            "COALESCE(FORMAT(simpanan_wajib.balance, 0, 'id_ID'), 0) wajib",
+            "COALESCE(FORMAT(simpanan_pokok.balance, 0, 'id_ID'), 0) pokok",
+            "COALESCE(FORMAT(simpanan_sukarela.balance, 0, 'id_ID'), 0) sukarela",
+            "COALESCE(FORMAT(simpanan_investasi.balance, 0, 'id_ID'), 0) investasi",
+            "FORMAT(
+                COALESCE(simpanan_wajib.balance, 0) + 
+                COALESCE(simpanan_pokok.balance, 0) + 
+                COALESCE(simpanan_sukarela.balance, 0) + 
+                COALESCE(simpanan_investasi.balance, 0), 
+            0, 'id_ID') total",
+        ])
+        ->from('person')
+        ->join('user', 'user.id = person.user_id', 'left')
+        ->join('depo', 'depo.id = person.depo', 'left')
+        ->join('position', 'position.id = person.position', 'left')
+        ->join('(SELECT id, person, SUM(balance) balance FROM simpanan_wajib GROUP BY person) simpanan_wajib', 'simpanan_wajib.person = person.nik', 'left')
+        ->join('(SELECT id, person, SUM(balance) balance FROM simpanan_pokok GROUP BY person) simpanan_pokok', 'simpanan_pokok.person = person.nik', 'left')
+        ->join('(SELECT id, person, SUM(balance) balance FROM simpanan_sukarela GROUP BY person) simpanan_sukarela', 'simpanan_sukarela.person = person.nik', 'left')
+        ->join('(SELECT id, person, SUM(balance) balance FROM simpanan_investasi GROUP BY person) simpanan_investasi', 'simpanan_investasi.person = person.nik', 'left')
+        ->where('user.role', '2')
+        ->order_by('person.id', 'asc');
+        
+        $q = $this->db->get();
+
+        return $q->result_array();
+    }
+
+
 }
 
