7320aeb22ad44b8841968ce9781f8eddf8067df8
faizin <nasrunfaizinkholiq@gmail.com>
2023-02-26 18:59:57 +0700

    update dashboard

--- application/models/Home_model.php
+++ application/models/Home_model.php
@@ -27,7 +27,7 @@ class Home_model extends CI_Model {
         $this->db->select([
             "CASE WHEN simpanan_pokok.total IS NOT NULL THEN CONCAT('RP', FORMAT(simpanan_pokok.total, 0, 'id_ID')) ELSE 0 END pokok",
             "CASE WHEN simpanan_wajib.total IS NOT NULL THEN CONCAT('RP', FORMAT(simpanan_wajib.total, 0, 'id_ID')) ELSE 0 END wajib",
-            "CASE WHEN simpanan_sukarela.total IS NOT NULL THEN CONCAT('RP', FORMAT(simpanan_sukarela.total, 0, 'id_ID')) ELSE 0 END sukarela",
+            "CASE WHEN simpanan_sukarela.total IS NOT NULL THEN CONCAT('RP', FORMAT(simpanan_sukarela.total - penarikan_sukarela.total, 0, 'id_ID')) ELSE 0 END sukarela",
             "CASE WHEN simpanan_investasi.total IS NOT NULL THEN CONCAT('RP', FORMAT(simpanan_investasi.total, 0, 'id_ID')) ELSE 0 END investasi",
             "0 pinjaman",
         ])
@@ -36,6 +36,7 @@ class Home_model extends CI_Model {
         ->join('(SELECT id, person, COALESCE(SUM(balance), 0) total FROM simpanan_wajib GROUP BY person) simpanan_wajib', 'simpanan_wajib.person = person.nik', 'left')
         ->join('(SELECT id, person, COALESCE(SUM(balance), 0) total FROM simpanan_sukarela GROUP BY person) simpanan_sukarela', 'simpanan_sukarela.person = person.nik', 'left')
         ->join('(SELECT id, person, COALESCE(SUM(balance), 0) total FROM simpanan_investasi GROUP BY person) simpanan_investasi', 'simpanan_investasi.person = person.nik', 'left')
+        ->join('(SELECT id, person, COALESCE(SUM(balance), 0) total FROM penarikan_simpanan WHERE type = "Sukarela" AND status = "Approved" GROUP BY person) penarikan_sukarela', 'penarikan_sukarela.person = person.nik', 'left')
         ->where('person.nik', $person);
         $data = $this->db->get()->row_array();
         
