ccce64056a728687d224f35af55f60f26ba95d8a
faizin <nasrunfaizinkholiq@gmail.com>
2023-03-28 21:18:16 +0700

    update migrations

--- migrations/040_pinjaman.sql
+++ migrations/040_pinjaman.sql
@@ -2,7 +2,7 @@ ALTER TABLE `pinjaman` ADD `year` VARCHAR(4) NOT NULL AFTER `person`, ADD `month
 
 ALTER TABLE `pinjaman` ADD `status` ENUM('Approved','Decline','Pending') NOT NULL AFTER `balance`;
 
-CREATE TABLE `koperassi`.`angsuran` (`id` INT NOT NULL AUTO_INCREMENT , `year` VARCHAR(4) NOT NULL , `month` VARCHAR(2) NOT NULL , `month_no` VARCHAR(2) NOT NULL , `pokok` FLOAT NOT NULL , `bunga` FLOAT NOT NULL , `status` ENUM('Approved','Decline','Pending') NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
+CREATE TABLE `angsuran` (`id` INT NOT NULL AUTO_INCREMENT , `year` VARCHAR(4) NOT NULL , `month` VARCHAR(2) NOT NULL , `month_no` VARCHAR(2) NOT NULL , `pokok` FLOAT NOT NULL , `bunga` FLOAT NOT NULL , `status` ENUM('Approved','Decline','Pending') NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
 
 ALTER TABLE `pinjaman` ADD `angsuran` INT NOT NULL AFTER `date`;
 
