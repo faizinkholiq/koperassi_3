e9e585cbc3a4d7c7e6ef487f85c49c327160f1f3
faizin <nasrunfaizinkholiq@gmail.com>
2023-04-16 17:05:16 +0700

    update dashboard

--- application/views/dashboard/admin.php
+++ application/views/dashboard/admin.php
@@ -1,3 +1,42 @@
+<style>
+    .card-link-dashboard{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        border:none; 
+        color:white; 
+        font-size:1.3rem; 
+        width:100%;
+        padding-right: 2rem;
+        transition: padding-right 0.5s;
+    }
+
+    .card-link-dashboard:hover{
+        text-decoration: none;
+        padding-right: 1.2rem;
+        color:white;
+    }
+
+    .card-link-red{
+        background: #c0392b;
+    }
+
+    .card-link-yellow{
+        background: #f39c12;
+    }
+
+    .card-link-blue{
+        background: #2980b9;
+    }
+
+    .card-link-green{
+        background: #16a085;
+    }
+
+    .card-link-purple{
+        background: #8e44ad;
+    }
+</style>
 <!-- Content Row -->
 <div class="row">
     <div class="col-xl-4 col-md-6 mb-4">
@@ -12,10 +51,10 @@
                     <div class="col-lg-4">Total Simpanan</div>
                 </div>
             </div>
-            <div class="card-footer font-weight-bold" style="background:#c0392b; border:none; color:white; font-size:1.3rem;">
+            <a href="<?= site_url('simpanan/page/pokok'); ?>" class="card-link-dashboard card-link-red card-footer font-weight-bold">
                 Simpanan Pokok
                 <i class="float-right fa fa-chevron-right"></i>
-            </div>
+            </a>
         </div>
     </div>
     <div class="col-xl-4 col-md-6 mb-4" >
@@ -30,10 +69,10 @@
                     <div class="col-lg-4">Total Simpanan</div>
                 </div>
             </div>
-            <div class="card-footer font-weight-bold" style="background:#f39c12; border:none; color:white; font-size:1.3rem;">
+            <a href="<?= site_url('simpanan/page/wajib'); ?>" class="card-link-dashboard card-link-yellow card-footer font-weight-bold">
                 Simpanan Wajib
                 <i class="float-right fa fa-chevron-right"></i>
-            </div>
+            </a>
         </div>
     </div>
     <div class="col-xl-4 col-md-6 mb-4">
@@ -48,10 +87,10 @@
                     <div class="col-lg-4">Total Simpanan</div>
                 </div>
             </div>
-            <div class="card-footer font-weight-bold" style="background:#2980b9; border:none; color:white; font-size:1.3rem;">
+            <a a href="<?= site_url('simpanan/page/sukarela'); ?>" class="card-link-dashboard card-link-blue card-footer font-weight-bold">
                 Simpanan Sukarela
                 <i class="float-right fa fa-chevron-right"></i>
-            </div>
+            </a>
         </div>
     </div>
     <div class="col-xl-4 col-md-6 mb-4 offset-2">
@@ -66,10 +105,10 @@
                     <div class="col-lg-4">Total Simpanan</div>
                 </div>
             </div>
-            <div class="card-footer font-weight-bold" style="background:#16a085; border:none; color:white; font-size:1.3rem;">
+            <a href="<?= site_url('simpanan/page/investasi'); ?>" class="card-link-dashboard card-link-green card-footer font-weight-bold">
                 Investasi
                 <i class="float-right fa fa-chevron-right"></i>
-            </div>
+            </a>
         </div>
     </div>
     <div class="col-xl-4 col-md-6 mb-4">
@@ -81,7 +120,7 @@
                     </div>
                 </div>
             </div>
-            <div class="card-footer font-weight-bold" style="background:#8e44ad; border:none; color:white; font-size:1.3rem;">
+            <div class="card-link-dashboard card-link-purple card-footer font-weight-bold">
                 Total Simpanan
             </div>
         </div>
